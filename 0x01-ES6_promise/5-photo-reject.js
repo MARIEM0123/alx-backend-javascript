8fdb74f3714d62ebe64cd457688abac27213d2e3
Mariem0123 <m00riemhissou@gmail.com>
2024-04-12 19:35:25 +0000

    js

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,4 +1,8 @@
-
-export default function uploadPhoto(filename) {
-  return Promise.reject(new Error(`${filename} cannot be processed`));
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve(
+    {
+      firstName,
+      lastName,
+    },
+  );
 }
